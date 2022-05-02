e3f194e6c8d5ec8ed1d9a6eebca7ee757bff10a7
Wellington Silva de Almeida <wellingtonsilvadealmeida1995@gmail.com>
2022-05-02 15:32:59 -0300

    Criação da pasta styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoadNameInput from './components/name'
 import LoadCountries from './components/countries'
-import './styles.css';
+import './styles/styles.css';
 
 function App() {
   return (
